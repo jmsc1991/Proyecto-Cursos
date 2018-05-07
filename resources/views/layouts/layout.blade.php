d285bea9fc48eed2c55eed92dabd2372ded135d4
jose <josesolanacases@gmail.com>
2018-05-07 10:39:23 +0200

    Añadida pagina Home

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,237 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Cursos DAW</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="{!! asset('template/css/bootstrap.css') !!}">
+    <link rel="stylesheet" href="{!! asset('template/css/animate.css') !!}">
+    <link rel="stylesheet" href="{!! asset('template/css/owl.carousel.min.css') !!}">
+
+    <link rel="stylesheet" href="{!! asset('template/fonts/ionicons/css/ionicons.min.css') !!}">
+    <link rel="stylesheet" href="{!! asset('template/fonts/fontawesome/css/font-awesome.min.css') !!}">
+    <link rel="stylesheet" href="{!! asset('template/fonts/flaticon/font/flaticon.css') !!}">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="{!! asset('template/css/style.css') !!}">
+</head>
+<body>
+
+<header role="banner">
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-light">
+        <div class="container">
+            <a class="navbar-brand absolute" href="index.html">Cursos DAW</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse navbar-light" id="navbarsExample05">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="#">Cursos Online</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categorias</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown05">
+                            <a class="dropdown-item" href="#">HTML</a>
+                            <a class="dropdown-item" href="#">WordPress</a>
+                            <a class="dropdown-item" href="#">Web Development</a>
+                            <a class="dropdown-item" href="#">Javascript</a>
+                            <a class="dropdown-item" href="#">Photoshop</a>
+                        </div>
+
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Acerca De</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav absolute-right">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Registrarse</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+</header>
+<!-- END header -->
+
+<section class="site-hero overlay" data-stellar-background-ratio="0.5" style="background-image: url(template/images/big_image_1.jpg);">
+    <div class="container">
+        <div class="row align-items-center site-hero-inner justify-content-center">
+            <div class="col-md-8 text-center">
+
+                <div class="mb-5 element-animate">
+                    <h1>Cursos y Video Tutoriales Online</h1>
+                    <p class="lead">Hazte miembro VIP para tener acceso a todos nuestros cursos y videos de forma ilimitada</p>
+                    <p><a href="#" class="btn btn-primary">Conseguir VIP</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END section -->
+
+<section class="site-section">
+    <div class="container">
+        <section class="school-features text-dark d-flex">
+            <div class="inner">
+                <div class="media d-block feature">
+                    <div class="icon"><span class="flaticon-video-call"></span></div>
+                    <div class="media-body">
+                        <h3 class="mt-0">Instructores Expertos</h3>
+                        <p>Aprenda de una forma sencilla con los mejores Profesores Online.</p>
+                    </div>
+                </div>
+
+                <div class="media d-block feature">
+                    <div class="icon"><span class="flaticon-student"></span></div>
+                    <div class="media-body">
+                        <h3 class="mt-0">Aprende en cualquier parte del mundo</h3>
+                        <p>Podras aprender con nuestros cursos online en cualquier parte del mundo.</p>
+                    </div>
+                </div>
+
+                <div class="media d-block feature">
+                    <div class="icon"><span class="flaticon-video-player-1"></span></div>
+                    <div class="media-body">
+                        <h3 class="mt-0">Videos creativos y practicos</h3>
+                        <p>Con nuestros cursos aprenderas de una forma creatica y practica.</p>
+                    </div>
+                </div>
+
+
+                <div class="media d-block feature">
+                    <div class="icon"><span class="flaticon-audiobook"></span></div>
+                    <div class="media-body">
+                        <h3 class="mt-0">Audio learning</h3>
+                        <p>Harum, adipisci, aspernatur. Vero repudiandae quos ab debitis, fugiat culpa obcaecati, voluptatibus ad distinctio cum soluta fugit sed animi eaque?</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</section>
+<!-- END section -->
+
+<section class="site-section bg-light">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-7 text-center">
+                <h2>Top Cursos</h2>
+                <p class="lead">Una lista de nuestros mejores cursos y video tutoriales.</p>
+            </div>
+        </div>
+        <div class="row top-course">
+            <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                <a href="#" class="course">
+                    <img src="template/images/webdesign.jpg" alt="Image placeholder">
+                    <h2>Web Design 101</h2>
+                    <p>Enroll Now</p>
+                </a>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                <a href="#" class="course">
+                    <img src="template/images/wordpress.jpg" alt="Image placeholder">
+                    <h2>Learn How To Develop WordPress Plugin</h2>
+                    <p>Enroll Now</p>
+                </a>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                <a href="#" class="course">
+                    <img src="template/images/javascript.jpg" alt="Image placeholder">
+                    <h2>JavaScript 101</h2>
+                    <p>Enroll Now</p>
+                </a>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                <a href="#" class="course">
+                    <img src="template/images/photoshop.jpg" alt="Image placeholder">
+                    <h2>Photoshop Design 101</h2>
+                    <p>Enroll Now</p>
+                </a>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                <a href="#" class="course">
+                    <img src="template/images/reactjs.jpg" alt="Image placeholder">
+                    <h2>Learn Native ReactJS</h2>
+                    <p>Enroll Now</p>
+                </a>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                <a href="#" class="course">
+                    <img src="template/images/angularjs.jpg" alt="Image placeholder">
+                    <h2>Learn AngularJS 2</h2>
+                    <p>Enroll Now</p>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END section -->
+
+<footer class="site-footer" style="background-image: url(template/images/big_image_3.jpg);">
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-md-4">
+                <h3>Acerca de nosotros</h3>
+                <p> Cursos DAW es una web desarrollada por Alvaro y Jose, dedicada a la gestión de Cursos y Videos online y realizada como proyecto de final de curso de DAW</p>
+            </div>
+            <div class="col-md-6 ml-auto">
+                <div class="row">
+                    <div class="col-md-4">
+                        <ul class="list-unstyled">
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Teachers</a></li>
+                            <li><a href="#">Courses</a></li>
+                            <li><a href="#">Categories</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- END footer -->
+
+<!-- loader -->
+<div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
+
+<script src="{!! asset('template/js/jquery-3.2.1.min.js') !!}"></script>
+<script src="{!! asset('template/js/jquery-migrate-3.0.0.js') !!}"></script>
+<script src="{!! asset('template/js/popper.min.js') !!}"></script>
+<script src="{!! asset('template/js/bootstrap.min.js') !!}"></script>
+<script src="{!! asset('template/js/owl.carousel.min.js') !!}"></script>
+<script src="{!! asset('template/js/jquery.waypoints.min.js') !!}"></script>
+<script src="{!! asset('template/js/jquery.stellar.min.js') !!}"></script>
+
+
+<script src="{!! asset('template/js/main.js') !!}"></script>
+</body>
+</html>
